0be67f5fb8e5c154f02c985947b47278c0949945
oluwafisayomi <wrld26993@gmail.com>
2022-11-02 13:05:00 +0100

    interface created,map,array

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent implements OnInit {
+  public todoitem = '';
+  public userArray: any = [];
+  // public user: any = {}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addtodo() {
+    let userDetails = { todoitem: this.todoitem };
+    this.userArray.push(userDetails);
+    console.log(this.userArray);
+  }
+
+  // getUser(id:any) {
+  //   this.user = this.userArray.find((user:any, index:any)=>index!=id);
+  // }
+
+  // edit(id:number) {
+  //   // let editDetails = { firstname: this.firstname, lastname: this.lastname, email: this.email, password: this.password };
+  //   // this.userArray[id] (editDetails);
+  // }
+
+  // delete(id:number) {
+  //   this.userArray = this.userArray.filter((user:any, index:any)=>index!=id);
+  // }
+}
